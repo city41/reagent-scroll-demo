98af21a9f8d403f59bffe4c8a630ab32eed0e33f
Matt Greer <matt.e.greer@gmail.com>
2016-04-03 13:33:41 -0700

    Upgrade clj and cljs to fix Chrome bug

--- project.clj
+++ project.clj
@@ -6,10 +6,10 @@
 
   :source-paths ["src/clj" "src/cljs"]
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [cljsjs/react "0.12.2-5"]
                  [reagent "0.5.0-alpha3"]
-                 [org.clojure/clojurescript "0.0-2913" :scope "provided"]
+                 [org.clojure/clojurescript "0.0-3308" :scope "provided"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [ring "1.3.2"]
                  [ring/ring-defaults "0.1.3"]
